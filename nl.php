f5fa56755b7c6629c247231bffff12e8f34c543d
mEsteban <mathieu.esteban@prestashop.com>
2015-09-18 11:33:46 +0200

    New translation file: NL

---
+++ nl.php
@@ -0,0 +1,21 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{a2hosting}prestashop>a2hosting_9bef82b0935eca4eb71ea81879e6db9f'] = 'A2 Hosting with TurboCache';
+$_MODULE['<{a2hosting}prestashop>a2hosting_14a0ebdfc4c5dc400a1320175885087a'] = '24/7/365 Hulp. Uiterst betrouwbaar. Zeer doeltreffende hosting-oplossing';
+$_MODULE['<{a2hosting}prestashop>configure_eb76afbd11563cbbfd81b7df1368f334'] = 'Niemand houdt van een trage website. A2 hosting kan die van u een zetje geven!';
+$_MODULE['<{a2hosting}prestashop>configure_16820ccf0bbd678b0ff9e53807650207'] = 'Turbo Servers - Tot';
+$_MODULE['<{a2hosting}prestashop>configure_6a9e3bb7048caa428a1747e20bae8289'] = '20X sneller';
+$_MODULE['<{a2hosting}prestashop>configure_42423eaea3ddbb89356a42b36ea760d1'] = 'in vergelijking met concurrerende PrestaShop Hosts';
+$_MODULE['<{a2hosting}prestashop>configure_6160988539537bac76f88332bc421b1e'] = 'PrestaShop';
+$_MODULE['<{a2hosting}prestashop>configure_b866ae21552c1156b4b0176e7c61408e'] = 'Voorgeïnstalleerd';
+$_MODULE['<{a2hosting}prestashop>configure_2ef50b4c466304dc6ac77bac8a779971'] = 'Exclusief';
+$_MODULE['<{a2hosting}prestashop>configure_ce6f949f37d0902b0f3a880746b3e589'] = 'SwiftCache Site versneller';
+$_MODULE['<{a2hosting}prestashop>configure_64ea186c14a7ebd84b02ff1fbe8a2a66'] = 'Prestaties Tuned SwiftServer Platform &';
+$_MODULE['<{a2hosting}prestashop>configure_8c7bb3181ffe6f4831aad7a230bebe93'] = 'Gratis SSD\'s';
+$_MODULE['<{a2hosting}prestashop>configure_af6f306d5e89b64ecb0b230c4fb5cbad'] = 'U kunt kiezen uit meerdere Datacenter locaties in de VS, Europa en Azië';
+$_MODULE['<{a2hosting}prestashop>configure_715bf70573792aeaa24ffc8671dde671'] = '1-Klik SSL-certificaat instellen';
+$_MODULE['<{a2hosting}prestashop>configure_cbe058b1ba61cd7597be103a917fd7ae'] = '24/7/365 Guru Crew Hulp';
+$_MODULE['<{a2hosting}prestashop>configure_25623606a328e9ef673d832dc4b30e64'] = 'https://partners.a2hosting.com/solutions.php?id=3682&url=311';
+$_MODULE['<{a2hosting}prestashop>configure_8f41f7c43322e04110d6d7583f763430'] = 'Schrijf u nu in!';
